3a835956416e2affdf71bd833aafe8ea895ff4d0
Hossam Elnaggar <hossam.elnaggar.ny@gmail.com>
2023-12-21 10:42:06 -0500

    add meta

--- node_execise/discover/script/videoControlMute.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $("video").prop('muted');
-    var vid = document.getElementById("video");
-    // while()
-    console.log(vid.readyState);
-    vid.play();
-    
-})
